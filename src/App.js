e14bef0ebadf0f259f39f23a86345b6b38ad6bc8
Samandar-Jumanov <samandarjumanovudemy@gmail.com>
2023-03-07 21:55:35 +0300

    scroll images

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css'
+import ImageSlider from './ImageSlider'
+const App = ()=>{
+  return (
+  <div className="App">
+    <ImageSlider/>
+  </div>
+  )
+}
+export default App
---
+++ src/ImageSlider.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import './ImageSlider.css'
+import image1 from './images/1.jpg'
+import image2 from './images/2.jpg'
+import image3 from './images/3.jpg'
+import image4 from './images/4.jpg'
+import {GrLinkNext} from 'react-icons/gr'
+import {BiArrowBack} from 'react-icons/bi'
+
+const ImagesSlider = () =>{
+    let  [image , setImage] = useState(0)
+    const Images = [ { id:1, url:image1 }, {id:2, url:image2},{ id:3, url:image3},{id:4,url:image4}]
+
+    setTimeout(()=>{
+      image <  Images.length -1  && setImage(image +1 )
+    }, 2000)
+     
+  
+     return(
+    <div className='imageSliderContainer'>
+        <h1 style={{color:'white'}}>Auto scroll and handle scrol images
+       <p>{Images[image].id} /{Images.length}</p>         
+         </h1> 
+    <div className="inner">
+     
+    <img src={Images[image].url} alt=""  className='image'/>
+   <br />
+   <br />
+   <button onClick={()=>{image> 0 && setImage(image-1)}} className='btn back'><BiArrowBack/></button>
+   <button onClick={()=>{image < Images.length - 1 && setImage(image+1)}} className='btn next'><GrLinkNext/></button>
+    </div>
+    </div>
+    )
+}
+export default ImagesSlider
